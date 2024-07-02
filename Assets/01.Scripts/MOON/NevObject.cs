9cdad2adb121139b90856387a6b1f0da50d760e8
moon0411 <157565761+moon080411@users.noreply.github.com>
2024-07-02 19:53:28 +0900

    fix:navigation Script name

--- Assets/01.Scripts/MOON/NevObject.cs
+++ Assets/01.Scripts/MOON/NevObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NevEnemy : MonoBehaviour
+public class NevObject : MonoBehaviour
 {
     public Transform target;
      float lerpSpeed = 15f;
