95198616b6ec70513bbc0f2ad44d0dd28ded6c1d
wannnhi <nanhi@gmail.com>
2024-07-11 18:03:28 +0900

    feat : TIMELINE

--- Assets/01.Scripts/YWH/Stage1/Stage1.cs
+++ Assets/01.Scripts/YWH/Stage1/Stage1.cs
@@ -10,14 +10,17 @@ public class Stage1 : MonoBehaviour
 
     void Start()
     {
-        FadeManager.instance.FadeOut(1);
-        ChatSystem.Instance.TypCoStart("금자월", "또.. 이런건가?", 0.2f);
-
+       
         StartCoroutine(MasterSay());
     }
 
     IEnumerator MasterSay()
     {
+       
+    
+        ChatSystem.Instance.TypCoStart("금자월", "또.. 이런건가?", 0.2f);
+        yield return new WaitUntil(() => ChatSystem.Instance.endText == true);
+        FadeManager.instance.FadeOut(1);
         PlayerManager.Instance.Player.PlayerInput._controls.Disable();
         yield return new WaitForSeconds(2);
         ChatSystem.Instance.StopTyp();
